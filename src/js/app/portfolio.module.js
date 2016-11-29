2a785c3a8fcfd1dd3abc9dd1fe4803b089f38031
mattgrosso <mattgrosso@gmail.com>
2016-11-29 15:00:23 -0500

    Well, I've got a cool mondrian effect going on. That's cool.

--- src/js/app/portfolio.module.js
+++ src/js/app/portfolio.module.js
@@ -1,6 +1,13 @@
 (function() {
   'use strict';
 
+  $('.move-stuff').on('click', function moveStuff() {
+      $('.moveable-canvas').css({
+        'top': '-100vh',
+        'left': '-150vw',
+      });
+  });
+
   angular
     .module('portfolio',['ui.router']);
 
