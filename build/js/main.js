9b6121c915caa075ff0bf76e16aa40b60ff8c343
mattgrosso <mattgrosso@gmail.com>
2016-12-12 22:27:47 -0500

    Organized some code and set up a conditional to catch hashes that are typed directly.

--- build/js/main.js
+++ build/js/main.js
@@ -1,6 +1,46 @@
 (function() {
   'use strict';
 
+  console.log(window.location);
+
+  if (window.location.hash === '') {
+    console.log('/');
+    $('.moveable-canvas').css({
+      'top': '-204vh',
+      'left': '-163vw',
+    });
+  } else if (window.location.hash === '#projects') {
+    console.log('projects');
+    $('.moveable-canvas').css({
+      'top': '-72vh',
+      'left': '-121vw',
+    });
+  } else if (window.location.hash === '#bootstrap') {
+    console.log('bootstrap');
+    $('.moveable-canvas').css({
+      'top': '-75vh',
+      'left': '-329vw',
+    });
+  } else if (window.location.hash === '#responsive') {
+    console.log('responsive');
+    $('.moveable-canvas').css({
+      'top': '-265vh',
+      'left': '-273vw',
+    });
+  } else if (window.location.hash === '#recommend') {
+    console.log('recommend');
+    $('.moveable-canvas').css({
+      'top': '-329vh',
+      'left': '-83vw',
+    });
+  } else if (window.location.hash === '#about') {
+    console.log('about');
+    $('.moveable-canvas').css({
+      'top': '-215vh',
+      'left': '-400vw',
+    });
+  }
+
   window.addEventListener('hashchange', function hashNav() {
     var hash = window.location.hash;
 
