be22cd13c1dde844dd6cbbf0f4b4cce1d663a46a
mattgrosso <mattgrosso@gmail.com>
2016-12-13 16:51:26 -0500

    Reformatted homepage nav and began to add background images.

--- build/js/main.js
+++ build/js/main.js
@@ -1,46 +1,73 @@
 (function() {
   'use strict';
 
+  var hashList = {
+    home: {
+      top: '-209vh',
+      left: '-163vw'
+    },
+    projects: {
+      top: '-72vh',
+      left: '-121vw'
+    },
+    bootstrap: {
+      top: '-75vh',
+      left: '-329vw'
+    },
+    responsive: {
+      top: '-265vh',
+      left: '-273vw'
+    },
+    recommend: {
+      top: '-329vh',
+      left: '-83vw'
+    },
+    about: {
+      top: '-215vh',
+      left: '-400vw'
+    }
+  }
+
   if (window.location.hash === '') {
     console.log('/');
     $('.moveable-canvas').css({
-      'top': '-204vh',
-      'left': '-163vw',
+      'top': hashList.home.top,
+      'left': hashList.home.left,
     });
   }
   else if (window.location.hash === '#projects') {
     console.log('projects');
     $('.moveable-canvas').css({
-      'top': '-72vh',
-      'left': '-121vw',
+      'top': hashList.projects.top,
+      'left': hashList.projects.left,
     });
   }
   else if (window.location.hash === '#bootstrap') {
     console.log('bootstrap');
     $('.moveable-canvas').css({
-      'top': '-75vh',
-      'left': '-329vw',
+      'top': hashList.bootstrap.top,
+      'left': hashList.bootstrap.left,
     });
   }
   else if (window.location.hash === '#responsive') {
     console.log('responsive');
     $('.moveable-canvas').css({
-      'top': '-265vh',
-      'left': '-273vw',
+      'top': hashList.responsive.top,
+      'left': hashList.responsive.left,
     });
   }
   else if (window.location.hash === '#recommend') {
     console.log('recommend');
     $('.moveable-canvas').css({
-      'top': '-329vh',
-      'left': '-83vw',
+      'top': hashList.recommend.top,
+      'left': hashList.recommend.left,
     });
   }
   else if (window.location.hash === '#about') {
     console.log('about');
     $('.moveable-canvas').css({
-      'top': '-215vh',
-      'left': '-400vw',
+      'top': hashList.about.top,
+      'left': hashList.about.left,
     });
   }
 
@@ -49,43 +76,43 @@
 
     if (hash === '') {
       $('.moveable-canvas').css({
-        'top': '-204vh',
-        'left': '-163vw',
+        'top': hashList.home.top,
+        'left': hashList.home.left,
       });
     }
     else if (hash === '#projects') {
       console.log('projects');
       $('.moveable-canvas').css({
-        'top': '-72vh',
-        'left': '-121vw',
+        'top': hashList.projects.top,
+        'left': hashList.projects.left,
       });
     }
     else if (hash === '#bootstrap') {
       console.log('bootstrap');
       $('.moveable-canvas').css({
-        'top': '-75vh',
-        'left': '-329vw',
+        'top': hashList.bootstrap.top,
+        'left': hashList.bootstrap.left,
       });
     }
     else if (hash === '#responsive') {
       console.log('responsive');
       $('.moveable-canvas').css({
-        'top': '-265vh',
-        'left': '-273vw',
+        'top': hashList.responsive.top,
+        'left': hashList.responsive.left,
       });
     }
     else if (hash === '#recommend') {
       console.log('recommend');
       $('.moveable-canvas').css({
-        'top': '-329vh',
-        'left': '-83vw',
+        'top': hashList.recommend.top,
+        'left': hashList.recommend.left,
       });
     }
     else if (hash === '#about') {
       console.log('about');
       $('.moveable-canvas').css({
-        'top': '-215vh',
-        'left': '-400vw',
+        'top': hashList.about.top,
+        'left': hashList.about.left,
       });
     }
 
